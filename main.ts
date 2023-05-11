9faf4cfe8e9de71d3ed9295391e08a0dbe6786b2
vaclavik-jakub <115144117+vaclavik-jakub@users.noreply.github.com>
2023-05-11 11:26:07 +0200

    haha

--- main.ts
+++ main.ts
@@ -1,40 +1,13 @@
-let x = 2
-let y = 2
-
-radio.setGroup(1)
+radio.setGroup(13)
+radio.setTransmitSerialNumber(true)
 
 
-let accX = 0
-let accY = 0
-let accZ = 0
-
-let calibrateX = accX
-let calibrateY = accY
-let calibrateZ = accZ
-
-let rX = 0
-let rY = 0
-let rZ = 0
-let block = false
-
-    accX = input.acceleration(Dimension.X)
-    accY = input.acceleration(Dimension.Y)
-
-input.onButtonPressed(Button.AB, function () {
-        calibrateX = accX
-        calibrateY = accY
-
-            basic.showIcon(IconNames.Duck)
-
-            accX = input.acceleration(Dimension.X)
-            accY = input.acceleration(Dimension.Y)
-
-        Math.round(calibrateX)
-        Math.round(calibrateY)
-
-        console.logValue("x", calibrateX)
-        console.logValue("y", calibrateY)
-        basic.clearScreen()
+input.onButtonPressed(Button.A, function () {
+    radio.sendValue("GO", 18)
 })
-
-
+input.onButtonPressed(Button.AB, function () {
+    radio.sendValue("STOP", 16)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendValue("BACK", 17)
+})
