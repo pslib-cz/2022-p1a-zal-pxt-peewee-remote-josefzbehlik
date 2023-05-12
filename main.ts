664e05f220936c96fadc65ac2b5466a45f4f5b5e
vaclavik-jakub <115144117+vaclavik-jakub@users.noreply.github.com>
2023-05-12 13:52:00 +0200

    mozna finish

--- main.ts
+++ main.ts
@@ -2,14 +2,32 @@ radio.setGroup(13)
 radio.setTransmitSerialNumber(true)
 
 
-let calibrateX = accX
+let accY = input.acceleration(Dimension.Y)
+let calibrateY = accY
+let center = 0
 
-input.onButtonPressed(Button.A, function () {
-    radio.sendValue("GO", 18)
-})
+//CENTRACE
 input.onButtonPressed(Button.AB, function () {
-    radio.sendValue("STOP", 16)
+    center = accY
+        basic.pause(500)
+    console.log(center)
+})
+
+//POSÍLÁNÍ ROZKAZU JET DO PŘEDU NEBO DO ZADU
+if(accY > center) {
+    radio.sendValue("GO", 18)
+}
+
+if(accY < center) {
+    radio.sendValue("BACK", 17)
+}
+
+//POSÍLÁNÍ ROZKAZU OTOČIT SE
+input.onButtonPressed(Button.A, function () {
+    radio.sendValue("LEFT", 16)
 })
 input.onButtonPressed(Button.B, function () {
-    radio.sendValue("BACK", 17)
+    radio.sendValue("RIGHT", 15)
 })
+
+
