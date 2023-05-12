6b3c847e922696e3efac785e45b6c3f41fe3d722
vaclavik-jakub <115144117+vaclavik-jakub@users.noreply.github.com>
2023-05-12 14:14:11 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,24 +1,29 @@
 radio.setGroup(13)
 radio.setTransmitSerialNumber(true)
 
+let accY = 0
+let center = accY
+let y = input.acceleration(Dimension.Y)
 
-let accY = input.acceleration(Dimension.Y)
-let calibrateY = accY
-let center = 0
+let block = false
 
-//CENTRACE
-input.onButtonPressed(Button.AB, function () {
-    center = accY
-        basic.pause(500)
-    console.log(center)
+basic.forever(function () {
+    accY = input.acceleration(Dimension.Y)
+
+        input.onButtonPressed(Button.AB, function () {
+            center = accY
+                accY = input.acceleration(Dimension.Y)
+                Math.round(center)
+            console.logValue("y", center)
+        })
 })
 
 //POSÍLÁNÍ ROZKAZU JET DO PŘEDU NEBO DO ZADU
-if(accY > center) {
+if (accY > center) {
     radio.sendValue("GO", 18)
 }
 
-if(accY < center) {
+if (accY < center) {
     radio.sendValue("BACK", 17)
 }
 
@@ -26,8 +31,7 @@ if(accY < center) {
 input.onButtonPressed(Button.A, function () {
     radio.sendValue("LEFT", 16)
 })
+
 input.onButtonPressed(Button.B, function () {
     radio.sendValue("RIGHT", 15)
 })
-
-
