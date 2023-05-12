2fe5e54eaf5272acc5ab49d7456c088e3aa178d9
vaclavik-jakub <115144117+vaclavik-jakub@users.noreply.github.com>
2023-05-12 14:25:33 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -5,33 +5,38 @@ let accY = 0
 let center = accY
 let y = input.acceleration(Dimension.Y)
 
-let block = false
+let controls = false
 
 basic.forever(function () {
     accY = input.acceleration(Dimension.Y)
 
         input.onButtonPressed(Button.AB, function () {
+            basic.pause(500)
             center = accY
                 accY = input.acceleration(Dimension.Y)
-                Math.round(center)
+                    Math.round(center)
+                controls = true
             console.logValue("y", center)
         })
 })
 
 //POSÍLÁNÍ ROZKAZU JET DO PŘEDU NEBO DO ZADU
-if (accY > center) {
-    radio.sendValue("GO", 18)
-}
+if (controls = true) {
+    if (y > center) {
+        radio.sendValue("GO", 18)
+    }
 
-if (accY < center) {
-    radio.sendValue("BACK", 17)
+    if (y < center) {
+        radio.sendValue("BACK", 17)
+    }
 }
 
 //POSÍLÁNÍ ROZKAZU OTOČIT SE
-input.onButtonPressed(Button.A, function () {
-    radio.sendValue("LEFT", 16)
+input.onButtonPressed(Button.B, function () {
+    radio.sendValue("RIGHT", 16)
 })
 
-input.onButtonPressed(Button.B, function () {
-    radio.sendValue("RIGHT", 15)
+//ZASTAVIT
+input.onButtonPressed(Button.A, function () {
+    radio.sendValue("STOP", 15)
 })
