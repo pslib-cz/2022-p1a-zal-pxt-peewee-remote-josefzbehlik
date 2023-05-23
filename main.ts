96500e28ca89015c599323a89fc43f7a9d675050
josefzbehlik <115144248+josefzbehlik@users.noreply.github.com>
2023-05-23 16:33:35 +0200

    jednodušší

--- main.ts
+++ main.ts
@@ -1,57 +1,9 @@
 radio.setGroup(13)
+radio.setTransmitPower(7)
 radio.setTransmitSerialNumber(true)
 
-    //OSA Y
-let jizdaY = 0
-let centraceY = jizdaY
-
-    //OSA X
-let jizdaX = 0
-let centraceX = jizdaX
-
-    //CENTRACE
-basic.forever(function() {
-    jizdaY = input.acceleration(Dimension.Y)
-    jizdaX = input.acceleration(Dimension.X)
-
-    console.log(jizdaY)
-    input.onButtonPressed(Button.AB, function() {
-        centraceY = jizdaY
-        centraceX = jizdaX
-    })
-
-    Math.round(centraceY)
-    Math.round(centraceX)
-    console.logValue("y", centraceY)
-
-    //JÍZDA 
-    if (jizdaY > centraceY) {
-        radio.sendValue("BACK", 14)
-    }
-    if (jizdaY < centraceY) {
-        radio.sendValue("GO", 13)
-    }
-
-    //OTÁČENÍ
-    if (jizdaX > centraceX) {
-        radio.sendValue("RIGHT", 15)
-    }
-    if (jizdaX < centraceX) {
-        radio.sendValue("LEFT", 16)
-    }
+basic.forever(function () {
+    let data = (input.acceleration(Dimension.X) + 1024).toString() + ";" + (input.acceleration(Dimension.Y) + 1024).toString()
+    //console.log(data)
+     radio.sendString(data)
 })
-
-    
-
-
-
-    
-
-
-
-    
-
-
-
-
-
