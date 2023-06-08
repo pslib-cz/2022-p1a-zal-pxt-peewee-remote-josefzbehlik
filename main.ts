92744a9a0036777052847f195019e7bcaafb4eea
josefzbehlik <115144248+josefzbehlik@users.noreply.github.com>
2023-06-08 11:17:10 +0200

    nefunguje lol

--- main.ts
+++ main.ts
@@ -1,19 +1,20 @@
+
+let data = ""
+let autoModeEnabled = false
 radio.setGroup(13)
 radio.setTransmitPower(7)
 radio.setTransmitSerialNumber(true)
-
+autoModeEnabled = true
 basic.forever(function () {
-    let data = (input.acceleration(Dimension.X) + 1024).toString() + ";" + (input.acceleration(Dimension.Y) + 1024).toString()
-    //console.log(data)
-     radio.sendString(data)
+    data = "" + (input.acceleration(Dimension.X) + 1024).toString() + ";" + (input.acceleration(Dimension.Y) + 1024).toString()
+    // console.log(data)
+    radio.sendString(data)
 })
-
-let autoModeEnabled = true
-input.onButtonPressed(Button.AB, function() {
-    if (autoModeEnabled === true){
+input.onButtonPressed(Button.AB, function () {
+    if (autoModeEnabled == true) {
         radio.sendNumber(0)
         autoModeEnabled = false
-    } else if (autoModeEnabled === false) {
+    } else if (autoModeEnabled == false) {
         radio.sendNumber(1)
         autoModeEnabled = true
     }
