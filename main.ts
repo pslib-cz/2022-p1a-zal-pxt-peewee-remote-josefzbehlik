3feeaca96e8886370473e5181ff03e1d1fc6333b
josefzbehlik <115144248+josefzbehlik@users.noreply.github.com>
2023-06-11 19:06:03 +0200

    ovladač ovladačuje a točí točidlem

--- main.ts
+++ main.ts
@@ -1,21 +1,34 @@
-
 let data = ""
 let autoModeEnabled = false
 radio.setGroup(13)
 radio.setTransmitPower(7)
 radio.setTransmitSerialNumber(true)
-autoModeEnabled = true
 basic.forever(function () {
     data = "" + (input.acceleration(Dimension.X) + 1024).toString() + ";" + (input.acceleration(Dimension.Y) + 1024).toString()
     // console.log(data)
     radio.sendString(data)
 })
-input.onButtonPressed(Button.AB, function () {
-    if (autoModeEnabled == true) {
-        radio.sendNumber(0)
-        autoModeEnabled = false
-    } else if (autoModeEnabled == false) {
-        radio.sendNumber(1)
-        autoModeEnabled = true
+input.onButtonPressed(Button.AB, function() {
+  if (autoModeEnabled === false){
+    radio.sendNumber(1)
+    autoModeEnabled = true}
+    else{
+          radio.sendNumber(0)
+          autoModeEnabled = false
     }
+  }  
+)
+input.onButtonPressed(Button.A, function () {
+radio.sendNumber(5)
 })
+input.onButtonPressed(Button.B, function () {
+radio.sendNumber(10)
+})
+
+
+
+
+
+
+
+
